c3a6c5d2a4db250c12b838a7c48623095e32aa9d
Jacob Greenway <jagreenway12@gmail.com>
2014-10-07 19:25:28 -0500

    added random page btn

---
+++ js/learn-more.js
@@ -0,0 +1,8 @@
+$("#landing-btn").click(function() {
+	var links = new Array("http://teamquadx.org/our-robot.html",
+						  "http://teamquadx.org/the-team.html",
+						  "http://teamquadx.org/events.html",
+						  "http://teamquadx.org/resources.html");
+
+	window.location.replace(links[Math.floor(Math.random() * 4)]);
+});
