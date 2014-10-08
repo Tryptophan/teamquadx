5be0eb68ee88fbbd8a52a2e28e675e5e0f735f4a
Jacob Greenway <jagreenway12@gmail.com>
2014-10-08 16:50:58 -0500

    formatted footer to be larger

--- js/learn-more.js
+++ js/learn-more.js
@@ -1,4 +1,4 @@
-$("#landing-btn").click(function() {
+$(".landing-btn").click(function() {
 	var links = new Array("http://teamquadx.org/our-robot.html",
 						  "http://teamquadx.org/the-team.html",
 						  "http://teamquadx.org/events.html",
