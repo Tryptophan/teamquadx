01a0256a1be3b156587bc6a034381eab498d8502
Jacob Greenway <jagreenway12@gmail.com>
2014-10-16 17:25:15 -0500

    updates

---
+++ js/features.js
@@ -0,0 +1,35 @@
+$(".robot-btn").click(function() {
+	$(".robot-des").css("display", "none");
+	$(".slide-des").css("display", "none");
+	$(".gyro-des").css("display", "none");
+	$(".wheel-des").css("display", "none");
+
+	$(".robot-des").css("display", "block");	
+});
+
+$("#slide-btn").click(function() {
+	$(".robot-des").css("display", "none");
+	$(".slide-des").css("display", "none");
+	$(".gyro-des").css("display", "none");
+	$(".wheel-des").css("display", "none");
+
+	$(".slide-des").css("display", "block");	
+});
+
+$("#gyro-btn").click(function() {
+	$(".robot-des").css("display", "none");
+	$(".slide-des").css("display", "none");
+	$(".gyro-des").css("display", "none");
+	$(".wheel-des").css("display", "none");
+
+	$(".gyro-des").css("display", "block");	
+});
+
+$("#wheel-btn").click(function() {
+	$(".robot-des").css("display", "none");
+	$(".slide-des").css("display", "none");
+	$(".gyro-des").css("display", "none");
+	$(".wheel-des").css("display", "none");
+
+	$(".wheel-des").css("display", "block");	
+});
