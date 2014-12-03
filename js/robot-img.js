1cfb80c694cbed66d8a0a880bbeef108b4d717f5
linnealovespie <linnealovespie@gmail.com>
2014-12-03 15:55:19 -0600

    HOVER?

---
+++ js/robot-img.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $("#entry-1").hover(function(){
+    $("#entry-1").css("tutorial-img, mentoring-slider-0");
+    },function(){
+    $("entry-1").css("entry-1");
+  });
+});
